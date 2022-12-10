84b14ccf4c1141a7700d6bb268e57c168b055f88
sadnessFM <oldschool_otaku@proton.me>
2022-12-10 18:07:06 +0300

    init

---
+++ The_end_of_Gydropon/API/Base.cs
@@ -0,0 +1,33 @@
+﻿using System.Data.SqlClient;
+
+namespace The_end_of_Gydropon.API
+{
+    public class Base
+    {
+        private static string CreateConnectionString(string userId, string password) =>
+            $"server=ip; database=db; user id={userId}; password={password};";
+        
+        public static string TakeTasks()
+        {
+            string task = null;
+            using (SqlConnection connection = 
+                   new SqlConnection(CreateConnectionString("3", "34")))
+            {
+                
+            }
+
+            return task;
+        }
+
+        public static string TakeTasks(string tasks)
+        {
+            string day = null;
+            using (SqlConnection connection = new SqlConnection(CreateConnectionString("3", "34")))
+            {
+                
+            }
+            
+            return day;
+        }
+    }
+}
---
+++ The_end_of_Gydropon/Tests/BasicTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using The_end_of_Gydropon.API;
+
+namespace The_end_of_Gydropon.Tests
+{
+    [TestFixture]
+    public class BasicTests
+    {
+        private string ConnectionString;
+
+        [Test, Description("My really really cool test")]
+        public void TestSpecificTasks()
+        {
+            Base.TakeTasks("sdfsd");
+        }
+    }
+}
