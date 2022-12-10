8e9b15042ecc497e070894f6d3ce767582f01f1f
sadnessFM <oldschool_otaku@proton.me>
2022-12-10 18:43:45 +0300

    хуетень

--- The_end_of_Gydropon/API/Base.cs
+++ The_end_of_Gydropon/API/Base.cs
@@ -1,33 +1,53 @@
-﻿using System.Data.SqlClient;
+﻿using System;
+using System.Data.SqlClient;
 
 namespace The_end_of_Gydropon.API
 {
     public class Base
     {
+        
+        
         private static string CreateConnectionString(string userId, string password) =>
             $"server=ip; database=db; user id={userId}; password={password};";
         
         public static string TakeTasks()
         {
-            string task = null;
-            using (SqlConnection connection = 
-                   new SqlConnection(CreateConnectionString("3", "34")))
+            try
             {
-                
+                string task = null;
+                using (SqlConnection connection = new SqlConnection(CreateConnectionString("3", "34")))
+                {
+
+                }
+
+                return task;
+            }
+            catch (Exception error)
+            {
+                // ignored
             }
 
-            return task;
+            return "0";
         }
 
         public static string TakeTasks(string tasks)
         {
-            string day = null;
-            using (SqlConnection connection = new SqlConnection(CreateConnectionString("3", "34")))
+            try
             {
-                
+                string task = null;
+                using (SqlConnection connection = new SqlConnection(CreateConnectionString("3", "34")))
+                {
+
+                }
+
+                return task;
             }
-            
-            return day;
+            catch (Exception error)
+            {
+                // ignored
+            }
+
+            return "0";
         }
     }
 }
