f3650cded96bbf9c9f802e1b18f9021c8d0c521a
sadnessFM <oldschool_otaku@proton.me>
2022-12-10 20:48:27 +0300

    база на ошибки и тесты к ним

--- The_end_of_Gydropon/ErrorMessages/BaseErrors.cs
+++ The_end_of_Gydropon/ErrorMessages/BaseErrors.cs
@@ -1,31 +1,53 @@
-﻿using System;
+﻿using System.IO;
 
 namespace The_end_of_Gydropon.ErrorMessages
 {
     public class BaseErrors
-    {
-        public string CallError404(string additionalInfo)
+    { 
+        private static string _errorLogPath = @"D:\log.txt";
+
+        public static void WriteLogMessage(string logMessage)
+        {
+            using (StreamWriter tw = new StreamWriter(_errorLogPath))
+            {
+                //logMessage = logMessage + " - on " + DateTime.Now.ToString(CultureInfo.InvariantCulture);
+                tw.Write(logMessage);
+                tw.Close();
+            }
+        }
+
+        #region ErrorMessages
+
+        private string CallError403(string additionalInfo)
+        {
+            return "Call Error 403 - Forbidden. " 
+                   + "\nAdditional info: " + additionalInfo;
+        }
+        
+        private string CallError404(string additionalInfo)
         {
             return "Call Error 404 - not found"
                    + "\nAdditional info: " + additionalInfo;
         }
         
-        public string CallError500(string additionalInfo)
+        private string CallError500(string additionalInfo)
         {
             return "Call Error 500 - internal server error"
                    + "\nAdditional info: " + additionalInfo;
         }
         
-        public string CallError503(string additionalInfo)
+        private string CallError503(string additionalInfo)
         {
             return "Call Error 503 - service unavailable"
                    + "\nAdditional info: " + additionalInfo;
         }
         
-        public string CallError504(string additionalInfo)
+        private string CallError504(string additionalInfo)
         {
             return "Call Error 504 - gateway timeout"
                    + "\nAdditional info: " + additionalInfo;
         }
+
+        #endregion
     }
 }
---
+++ The_end_of_Gydropon/Tests/ErrorTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace The_end_of_Gydropon.Tests
+{
+    [TestFixture]
+    public class ErrorTests
+    {
+        [Test]
+        public void TestSpecificTasks()
+        {
+            string testText = "bebeeb";
+            ErrorMessages.BaseErrors.WriteLogMessage(testText);
+            string logText = System.IO.File.ReadAllText(@"D:\log.txt");
+            Assert.AreEqual(testText, logText);
+        }
+    }
+}
