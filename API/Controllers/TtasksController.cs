de3e051557eb18b9b22acfa8a9973fc361fd2d6c
goldman2g5 <bebra@DESKTOP-FHKBLG2>
2022-12-13 15:12:28 +0300

    пися

--- API/Controllers/TtasksController.cs
+++ API/Controllers/TtasksController.cs
@@ -1,6 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+﻿using System.Data.SqlClient;
 using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -31,64 +32,16 @@ namespace API.Controllers
             return ttask;
         }
 
-        // PUT: api/Ttasks/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutTtask(int id, Ttask ttask)
+        [HttpGet, Route("api/TestProcedure/{country}")]
+        public List<Ttask> GetTasks(string country)
         {
-            if (id != ttask.IdTtask)
+            var parameter = new SqlParameter
             {
-                return BadRequest();
-            }
+                ParameterName = "Country",
+                Value = "USA"
+            };
 
-            _context.Entry(ttask).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!TtaskExists(id))
-                {
-                    return NotFound();
-                }
-                throw;
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Ttasks
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Ttask>> PostTtask(Ttask ttask)
-        {
-            _context.Ttasks.Add(ttask);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetTtask", new { id = ttask.IdTtask }, ttask);
-        }
-
-        // DELETE: api/Ttasks/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteTtask(int id)
-        {
-            var ttask = await _context.Ttasks.FindAsync(id);
-            if (ttask == null)
-            {
-                return NotFound();
-            }
-
-            _context.Ttasks.Remove(ttask);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        private bool TtaskExists(int id)
-        {
-            return _context.Ttasks.Any(e => e.IdTtask == id);
+            var countries = _context.web_api_Test.SqlQuery<web_api_Test>("exec FindPeople @Country", parameter).ToList<web_api_Test>();
         }
     }
 }
