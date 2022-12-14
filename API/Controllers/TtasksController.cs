a9da0c1798546ccc6db3784b978e025b6f4512cf
sadnessFM <oldschool_otaku@proton.me>
2022-12-14 12:01:07 +0300

    будет время выебу сучара хранимая

--- API/Controllers/TtasksController.cs
+++ API/Controllers/TtasksController.cs
@@ -1,58 +1,40 @@
 ﻿using API.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
-using Newtonsoft.Json.Linq;
-using System.Net;
-using System.Text;
 
-namespace API.Controllers
+namespace API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TtasksController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class TtasksController : ControllerBase
+    private readonly AgronomicAppTestUserContext _context = new();
+
+    // GET: api/Ttasks
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Ttask>>> GetTtasks()
     {
-        private readonly AgronomicAppTestUserContext _context = new();
+        return await _context.Ttasks.ToListAsync();
+    }
 
-        // GET: api/Ttasks
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Ttask>>> GetTtasks()
+    // GET: api/Ttasks/5
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<Ttask>> GetTtask(int id)
+    {
+        Ttask? ttask = await _context.Ttasks.FindAsync(id);
+
+        if (ttask == null)
         {
-            return await _context.Ttasks.ToListAsync();
+            return NotFound();
         }
 
-        // GET: api/Ttasks/5
-        [HttpGet("{id:int}")]
-        public async Task<ActionResult<Ttask>> GetTtask(int id)
-        {
-            var ttask = await _context.Ttasks.FindAsync(id);
+        return ttask;
+    }
 
-            if (ttask == null)
-            {
-                return NotFound();
-            }
-
-            return ttask;
-        }
-
-        [HttpGet, Route("GetTasksProcedure")]
-        public HttpResponseMessage GetTasksProcedure(int? id)
-        {
-            try
-            {
-                var taskId = new SqlParameter("@Ttask_id", id);
-                var courseList = _context.Database.SqlQuery<Ttask>($"EXEC Get_Ttask_Card @ID_Ttask={taskId.Value}")
-                    .ToList();
-                return new HttpResponseMessage(HttpStatusCode.OK)
-                {
-                    Content = new StringContent(JArray.FromObject(courseList).ToString(), Encoding.UTF8,
-                        "application/json")
-                };
-            }
-            catch
-            {
-                return new HttpResponseMessage(HttpStatusCode.BadRequest);
-            }
-        }
+    [HttpGet, Route("GetTasksProcedure")]
+    public IActionResult GetTasksProcedure([FromQuery] int? id)
+    {
+        var result = _context.Ttasks.FromSql($"EXEC Get_Ttask_Card {id}").ToList();
+        return Ok(result);
     }
 }
